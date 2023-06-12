aaf85d0ce670ff3e530892111dc3a9f7eb5b6d94
diego andres omegna <domegna@gmail.com>
2023-06-12 14:56:56 +0200

    Tailwind and Devtools running

--- baseshare/pages/index.vue
+++ baseshare/pages/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-<h1>Helooooo</h1>
+  <div class="min-h-screen bg-gray-900 text-white">
+    <header class="container mx-auto p-4">
+      <h1 class="text-4xl font-semibold text-green-400">Hello World</h1>
+    </header>
+    <section class="container mx-auto p-4">
+      <p class="text-lg text-gray-300">
+        BaseShare with Nuxt3 and Tailwind CSS!
+      </p>
+    </section>
   </div>
 </template>
