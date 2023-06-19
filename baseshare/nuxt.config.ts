59dac8ecc6b833821e629696436795c2ee10a167
diego andres omegna <domegna@gmail.com>
2023-06-19 16:56:49 +0200

    ADD: strapi app and strapi extension

--- baseshare/nuxt.config.ts
+++ baseshare/nuxt.config.ts
@@ -1,8 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/devtools',
-  ],
+  modules: ['@nuxt/devtools', '@nuxtjs/strapi'],
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   postcss: {
@@ -10,5 +8,12 @@ export default defineNuxtConfig({
       tailwindcss: {},
       autoprefixer: {},
     },
+  },
+  strapi: {
+    url: process.env.STRAPI_URL || 'http://localhost:1337',
+    prefix: '/api',
+    version: 'v4',
+    cookie: {},
+    cookieName: 'strapi_jwt',
   },
 })
